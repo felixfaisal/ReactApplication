f8f0683e990b1ce88ea3770d5a6ef6f44844e263
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-12-07 08:33:09 +0530

    Added one component for testing and understanding file structure

---
+++ src/Catalog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Catalog.css'
+class Catalog extends React.Component{
+    render(){
+        return <div><h1>Felix is Awesome</h1></div>
+    }
+}
+
+export default Catalog;
