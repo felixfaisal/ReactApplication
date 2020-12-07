d755eaaf89e7a5972bd14bdf2058ce001b0b718d
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-12-07 08:36:39 +0530

    Tested component initializing

--- src/Catalog.js
+++ src/Catalog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Catalog.css'
 class Catalog extends React.Component{
     render(){
+        console.log("Catalog component initialized")
         return <div><h1>Felix is Awesome</h1></div>
     }
 }
