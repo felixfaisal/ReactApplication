42cc5bd56121b0d9aa8f89daa0d3118d737d2831
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-12-10 11:46:38 +0530

    Updated the about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -9,7 +9,7 @@ class AboutMe extends React.Component{
         return (
         <Grommet theme={grommet}>
         <Grid
-          rows={['200px', '80vh']}
+          rows={['100px', '80vh']}
           columns={['1/2', '1/2']}
           areas={[
             ['header', 'header'],
@@ -26,12 +26,14 @@ class AboutMe extends React.Component{
   
           <Box background="white" gridArea="main" justify="center" overflow="hidden">
           <Box height="60vh" width="100%" overflow="hidden" fill  align="center" justify="start" >
-            <Heading size="small" color="black"> I am amazing
-            
-            
-            
-            
+            <Heading size="small" color="black"> 
+            My name is Faisal Ahmed Farooq, I'm a student studying B.Tech in Presidency University.  </Heading>
+            <Heading size="small" color="black"> 
+            I have a good understanding of microservices architecture and familiar with Django and React for frontend and backend.
             </Heading>
+            <Heading size="small" color="black"> 
+            </Heading>
+            
            
           </Box>
           </Box>
